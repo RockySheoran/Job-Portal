4286fd51c8d605fb9547017a23fb3dc2675c74b0
RockySheoran <rockysheoran72@google.com>
2025-01-04 13:14:53 +0530

    2 commit

--- Frontend/src/utils/apiHead.jsx
+++ Frontend/src/utils/apiHead.jsx
@@ -1,7 +1,7 @@
 const backend_url =
   window.location.hostname === "localhost"
     ? "http://localhost:8000"
-    : " https://fooddel-backend3.onrender.com";
+    : "https://job-portal-q5bb.onrender.com";
 export const USER_API_END_POINT = `${backend_url}/api/v1/user`;
 export const JOB_API_END_POINT = `${backend_url}/api/v1/job`;
 export const APPLICATION_API_END_POINT = `${backend_url}/api/v1/application`;
